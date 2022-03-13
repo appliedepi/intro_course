838d74338730bc79631da22b3b39a19f2d6525c1
Neale Batra <neale.batra@gmail.com>
2022-03-13 19:51:51 -0400

    slides link

---
+++ modules/precourse/packages_to_install.R
@@ -0,0 +1,60 @@
+
+# Essential R packages for the course "Introduction to R for applied epidemiology"
+# Offered by Applied Epi
+
+# If you encounter errors when trying to run this script, 
+# contact your instructor of point-of-contact.
+
+
+# Ensures the package "pacman" is installed
+if (!require("pacman")) install.packages("pacman")
+
+
+# Packages available from CRAN
+##############################
+pacman::p_load(
+     
+     # learning R
+     ############
+     learnr,   # interactive tutorials
+     
+     # project and file management
+     #############################
+     here,     # file paths relative to R project root folder
+     rio,      # import/export of many types of data
+
+     # package install and management
+     ################################
+     remotes,  # install from github
+     
+     # General data management
+     #########################
+     tidyverse,    # includes many packages for tidy data wrangling and presentation
+     epitrix,      # Useful epi functions
+     
+     
+     # statistics  
+     ############
+     janitor,      # tables and data cleaning
+     gtsummary,    # making descriptive and statistical tables
+
+     # plots - general
+     #################
+     #ggplot2,         # included in tidyverse
+     cowplot,          # combining plots  
+     RColorBrewer,     # color scales
+     gghighlight,      # highlight a subset
+     ggrepel,          # smart labels
+     ggExtra,          # fancy plots  
+     scales,           # helper functions
+     apyramid,         # age and sex pyramids
+     
+     # routine reports
+     #################
+     rmarkdown,        # produce PDFs, Word Documents, Powerpoints, and HTML files
+
+     # tables for presentation
+     #########################
+     knitr,            # R Markdown report generation and html tables
+     flextable)        # HTML tables
+     
