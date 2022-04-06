5ee2037c6421cad331bc0cb47b841cfa51202985
Neale Batra <neale.batra@gmail.com>
2022-04-05 21:31:16 -0400

    flextable demo update

--- intro_course/scripts/backup/4_ebola_flextable.R
+++ intro_course/scripts/backup/4_ebola_flextable.R
@@ -1,25 +1,25 @@
-border_style = officer::fp_border(color="black", width=1)
 
 
 # Packages ----------------------------------------------------------------
-
 pacman::p_load(
   rio,            # import/export
   here,           # file pathways
-  flextable,      # make HTML tables 
+  flextable,      # make HTML tables
+  scales,         # helper functions
   officer,        # helper functions for nice tables
   tidyverse)      # data management, summary, and visualization
 
 
 # Import data# -------------------------------------------------------------
-
-
 surv <- import(here("data", "clean", "backup", "surveillance_linelist_clean_20141201.rds"))
 
 
-# Create table -----------------------------------------------------------
+# define border style -----------------------------------------------------
+border_style = officer::fp_border(color="black", width=1)
 
-vom_table <- surv %>% 
+
+# Create table -----------------------------------------------------------
+vom_ftable <- surv %>% 
   group_by(hospital) %>%                                     # get statistics for each hospital
   summarise(
     n_cases   = n(),                                         # number of rows (cases)
@@ -28,34 +28,35 @@ vom_table <- surv %>%
   flextable::qflextable()
 
 # formatting --------------------------------------------------------------
-# Note the flextable object is refreshed throughout (rather than one big pipe) so that you can print the object and check how it looks in stages
+# Note the flextable object is refreshed throughout (rather than one big pipe)
+# so that you can print the object and check how it looks in stages
 
 # Add extra header
-vom_ftable <- vom_table %>% 
+vom_ftable <- vom_ftable %>% 
   add_header_row(
-    top = TRUE,                          # New header goes on top of existing header row
-    values = c("Hospital",     
+    top = TRUE,                          # new header on top of existing header
+    colwidths = c(1,1,2),                # span the 4 columns
+    values = c("Hospital",               # values for new header
                "Total cases", 
-               "Cases with vomiting",    # This will be the top-level header for this and two next columns   
-               ""))                      # This can be blank as will be merged with column 3 later anyway
+               "Cases with vomiting"))   # This will span 2 columns
 
 # Merge cells and fix alignment
 vom_ftable <- vom_ftable  %>% 
-  merge_at(i = 1, j = 3:4, part = "header") %>%                         # Horizontally merge columns 3 to 4 in new header row
-  flextable::align(., align = "center", j = c(2:4), part = "all") %>%   # Centre text in columns 2-4
-  merge_at(i = 1:2, j = 1, part = "header")                             # Merge vertically the 'hospital' title in column 1
+  align(part = "all", align = "center", j = c(2:4)) %>%   # center text in columns 2-4
+  merge_at(i = 1:2, j = 1, part = "header")               # merge vertically the 'hospital' title in column 1
 
 # Re-label secondary headers
 vom_ftable <- vom_ftable  %>% 
-  set_header_labels(hospital = "", 
+  set_header_labels(hospital  = "", 
                     n_cases   = "No.",                                         
-                    vomit_n   = "No.",                # Note that this can be the same label as a different column!           
+                    vomit_n   = "No.",                           
                     vomit_pct = "Percent of total")
 
 
-# Change background for specific columns of interest - say we want the reader to focus on % vomiting
+# Change background for specific columns of interest
+# - for example if we want the reader to focus on percent vomiting
 vom_ftable <- vom_ftable  %>% 
-  bg(., j=4, part = "all", bg = "gray95")  # Highlight the percentage column with j=4 (note it doesn't colour the title header as it is a merged cell and not recognised as 4 )
+  bg(part = "all", j=4, bg = "gray95")  # Highlight the 4th column
 
 # Change background conditionally - say we want to highlight when >= 50% of cases have vomiting
 vom_ftable <- vom_ftable  %>% 
@@ -67,10 +68,5 @@ vom_ftable <- vom_ftable  %>%
 
 # Save
 save_as_docx(vom_ftable, path = here("outputs", "hospital_table.docx"))
-
-
-
-
-
 
 
